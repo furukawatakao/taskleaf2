60cc284610914e2d95007da43283c973b27bf1fd
furukawatakao <furukawatakao0926@gmail.com>
2019-07-11 21:15:46 +0900

    ４章完了

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   skip_before_action :login_required
-  
+
   def new
   end
 
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
 
     if user&.authenticate(session_params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, notice;ログインしました。
+      redirect_to root_url, notice:'ログインしました。'
     else
       render:new
     end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,8 @@
 class TasksController < ApplicationController
+before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
-    @tasks = current_user.tasks
+    @tasks = current_user.tasks.order(created_at: :desc)
   end
 
   def show
@@ -38,7 +40,13 @@ class TasksController < ApplicationController
   end
 
   private
+
     def task_params
       params.require(:task).permit(:name, :description)
     end
+
+    def set_task
+      @task = current_user.tasks.find(params[:id])
+    end
+    
 end
